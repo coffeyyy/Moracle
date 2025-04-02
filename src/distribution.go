a0384fd4dd8c636dbc38a385455ef3d068f91580
coffeyyy <46061020+coffeyyy@users.noreply.github.com>
2025-04-02 13:54:03 -0400

    added deribit api call

--- src/distribution.go
+++ src/distribution.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	
+	"math"
+	"time"
 )
 
-/*
-func calculate_standard_deviation(float mean, int size, float [30]prices) float64 {
+func calculate_standard_deviation(float mean, int size, prices map[string]int) float {
 	sum_square_diffs := 0
 	for _, price := range prices {
 		sum_square_diffs += ((prices[i] - mean) ** 2)
@@ -13,11 +13,10 @@ func calculate_standard_deviation(float mean, int size, float [30]prices) float6
 	return math.Sqrt(sum_square_diffs/size-1)
 }
 
-func calculate_mean(arr [30]prices, int size) float64 {
+func calculate_mean(prices map[string]int, int size) float {
 	sum := 0
 	for _, price := range prices {
 		sum += price
 	}
 	return sum/size
 }
-*/
